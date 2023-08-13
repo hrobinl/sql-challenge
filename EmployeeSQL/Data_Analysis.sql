0ff562ba831da57ab5e5bab044582bd55bcf538e
hrobinl <132854695+hrobinl@users.noreply.github.com>
2023-08-12 20:56:10 -0400

    #

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -1,1 +1,8 @@
 ﻿#List the employee number, last name, first name, sex, and salary of each employee.
+
+SELECT employees.emp_no AS "employee number", employees.last_name AS "last name",
+	   employees.first_name AS "first name", employees.sex AS "Sex",
+	   salaries.salary AS "salary"
+FROM employees
+	JOIN salaries
+	ON (employees.emp_no = salaries.emp_no);
