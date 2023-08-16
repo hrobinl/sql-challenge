97d8a363cdbc15348e81c5e24a042020fd648b6d
hrobinl <132854695+hrobinl@users.noreply.github.com>
2023-08-15 22:37:42 -0400

    adding code

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -1,8 +1,46 @@
-﻿#List the employee number, last name, first name, sex, and salary of each employee.
+﻿--- Create a list with employee details including: employee number, last name, first name, sex and salary
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
 
-SELECT employees.emp_no AS "employee number", employees.last_name AS "last name",
-	   employees.first_name AS "first name", employees.sex AS "Sex",
-	   salaries.salary AS "salary"
+--- Create a list for 1986 employee hire date including: first name, last name and hire date
+SELECT first_name, last_name, hire_date
 FROM employees
-	JOIN salaries
-	ON (employees.emp_no = salaries.emp_no);
+WHERE YEAR(hire_date) = 1986;
+
+--- Create a list including: manager info and department info
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+--- Create a list including: department number and employee details
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+--- Create a list of employees: names Hercules and with last name starting with a 'B'
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--- Create a list of employees in the: "Sales department"
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+--- Create a list of employees in both: "Sales" and "Development" departments
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+--- Create a list of grouped last names to visualize the frequency
+SELECT last_name, COUNT(*) as name_count
+FROM employees
+GROUP BY last_name
+ORDER BY name_count DESC;
--- EmployeeSQL/Data_Engineering.sql
+++ EmployeeSQL/Data_Engineering.sql
@@ -1,1 +1,49 @@
-﻿
+﻿--- Create a "Departments" table
+CREATE TABLE departments (
+    dept_no CHAR(4) NOT NULL PRIMARY KEY,
+    dept_name VARCHAR(255) NOT NULL
+);
+
+--- Create a "Dept_emp" table
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no CHAR(4) NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+--- Create a "Dept_manager" table
+CREATE TABLE dept_manager (
+    dept_no CHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
+
+---  Create an "Employees" table
+CREATE TABLE employees (
+    emp_no INT NOT NULL PRIMARY KEY,
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    sex ENUM('M', 'F') NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+--- Create a "Salaries" table
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    PRIMARY KEY (emp_no, salary)
+);
+
+--- Create a "Titles" table
+CREATE TABLE titles (
+    title_id VARCHAR(10) NOT NULL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
